33bfba4255318546f5b85dcdb27a438fcad2cebf
eugep <eu_genio@live.it>
2025-12-09 23:18:02 +0100

    new function

--- import_statistics.py
+++ import_statistics.py
@@ -5,44 +5,60 @@ from decimal import Decimal
 import sqlite3
 
 
-def gas(filename, state_metadata_id, statistic_metadata_id: int):
+def gas(filename, state_metadata_id, statistics_metadata_id: int):
     DATE_KEY = "DATA LETTURA"
-    DATE_FORMAT = "%Y-%m-%d"
-    state, sum = get_latest_state_and_sum(metadata_id=statistic_metadata_id)
-
     with open(filename, "r") as f:
         lines = list(csv.DictReader(f, delimiter=";"))
         lines.sort(key=lambda l: l.get(DATE_KEY, ""))
-        for i in range(len(lines)):
-            try:
-                from_date = datetime.strptime(lines[i][DATE_KEY], DATE_FORMAT)
-                lettura = Decimal(lines[i]["LETTURA"].lstrip("0"))
-            except:
-                continue
+        process_lines(
+            lines=lines,
+            statistics_metadata_id=statistics_metadata_id,
+            state_metadata_id=state_metadata_id,
+            date_key=DATE_KEY,
+            date_format="%Y-%m-%d",
+            lettura_key="LETTURA",
+        )
 
-            if lettura <= state:
-                continue
-            sum += lettura - state
-            state = lettura
-            try:
-                to_date = datetime.strptime(lines[i + 1][DATE_KEY], DATE_FORMAT)
-            except IndexError:
-                to_date = datetime.now()
 
-            run_sql(
-                state=lettura,
-                sum=sum,
-                statistic_metadata_id=statistic_metadata_id,
-                state_metadata_id=state_metadata_id,
-                from_date=from_date,
-                to_date=to_date,
-            )
+def process_lines(
+    lines: list[dict],
+    statistics_metadata_id: int,
+    state_metadata_id: int,
+    date_key: str,
+    date_format: str,
+    lettura_key: str,
+):
+    state, sum = get_latest_state_and_sum(statistics_metadata_id=statistics_metadata_id)
+    for i in range(len(lines)):
+        try:
+            from_date = datetime.strptime(lines[i][date_key], date_format)
+            lettura = Decimal(lines[i][lettura_key].lstrip("0"))
+        except:
+            continue
+
+        if lettura < state:
+            continue
+        sum += lettura - state
+        state = lettura
+        try:
+            to_date = datetime.strptime(lines[i + 1][date_key], date_format)
+        except IndexError:
+            to_date = datetime.now()
+
+        run_sql(
+            state=lettura,
+            sum=sum,
+            statistics_metadata_id=statistics_metadata_id,
+            state_metadata_id=state_metadata_id,
+            from_date=from_date,
+            to_date=to_date,
+        )
 
 
 def luce_giornaliera(
     filename,
     state_meta_ids: list[int],
-    statistic_meta_ids: list[int],
+    statistics_meta_ids: list[int],
     update_since: datetime = datetime.now(),
 ):
     DATE_KEY = "data_lettura"
@@ -52,33 +68,17 @@ def luce_giornaliera(
     with open(filename, "r") as f:
         lines = list(csv.DictReader(f, delimiter=";"))
         lines.sort(key=lambda l: datetime.strptime(l[DATE_KEY], DATE_FORMAT))
-        for fascia, statistic_metadata_id, state_metadata_id in zip(
-            FASCE, statistic_meta_ids, state_meta_ids
+        for fascia, statistics_metadata_id, state_metadata_id in zip(
+            FASCE, statistics_meta_ids, state_meta_ids
         ):
-            state, sum = get_latest_state_and_sum(
-                metadata_id=statistic_metadata_id, date_limit=update_since
+            process_lines(
+                lines=lines,
+                statistics_metadata_id=statistics_metadata_id,
+                state_metadata_id=state_metadata_id,
+                date_key=DATE_KEY,
+                date_format="%Y-%m-%d",
+                lettura_key=f"lettura_f{fascia}",
             )
-
-            for i in range(len(lines)):
-                from_date = datetime.strptime(lines[i][DATE_KEY], DATE_FORMAT)
-                lettura = Decimal(lines[i][f"lettura_f{fascia}"])
-
-                if from_date < update_since:
-                    continue
-                sum += lettura - state
-                state = lettura
-                try:
-                    to_date = datetime.strptime(lines[i + 1][DATE_KEY], DATE_FORMAT)
-                except IndexError:
-                    to_date = datetime.now()
-                run_sql(
-                    state=lettura,
-                    sum=sum,
-                    statistic_metadata_id=statistic_metadata_id,
-                    state_metadata_id=state_metadata_id,
-                    from_date=from_date,
-                    to_date=to_date,
-                )
 
 
 def update_state(
@@ -106,7 +106,7 @@ def update_state(
 def run_sql(
     state: Decimal,
     sum: Decimal,
-    statistic_metadata_id: int,
+    statistics_metadata_id: int,
     state_metadata_id: int,
     from_date: datetime,
     to_date: datetime = datetime.now(),
@@ -117,7 +117,7 @@ def run_sql(
                 table,
                 state,
                 sum,
-                statistic_metadata_id,
+                statistics_metadata_id,
                 from_date.timestamp(),
                 to_date.timestamp(),
             )
@@ -131,42 +131,15 @@ def run_sql(
 
 
 def get_latest_state_and_sum(
-    metadata_id: int, date_limit: datetime = datetime.now()
+    statistics_metadata_id: int, date_limit: datetime = datetime.now()
 ) -> tuple[Decimal, Decimal]:
     res = cur.execute(
         'SELECT state, "sum" FROM statistics WHERE metadata_id = {} AND start_ts < {} ORDER BY id DESC LIMIT 1;'.format(
-            metadata_id, date_limit.timestamp()
+            statistics_metadata_id, date_limit.timestamp()
         )
     )
     state, sum = res.fetchone()
     return Decimal(str(state)), Decimal(str(sum))
-
-
-def process_letture(
-    letture: list[tuple[datetime, Decimal]],
-    statistic_metadata_id: int,
-    state_metadata_id: int,
-    state: Decimal = Decimal(0),
-    sum: Decimal = Decimal(0),
-):
-    for i in range(len(letture)):
-        if letture[i][1] <= state:
-            continue
-        sum += letture[i][1] - state
-        state = letture[i][1]
-        from_date = letture[i][0]
-        try:
-            to_date = letture[i + 1][0]
-        except IndexError:
-            to_date = datetime.now()
-        run_sql(
-            state=state,
-            sum=sum,
-            statistic_metadata_id=statistic_metadata_id,
-            state_metadata_id=state_metadata_id,
-            from_date=from_date,
-            to_date=to_date,
-        )
 
 
 if __name__ == "__main__":
@@ -186,13 +159,13 @@ if __name__ == "__main__":
         headers = f.readline()
         if headers.startswith("PDR"):
             print(f"Importing GAS statistics from '{args.csv}'")
-            gas(args.csv, state_metadata_id=259, statistic_metadata_id=18)
+            gas(args.csv, state_metadata_id=259, statistics_metadata_id=18)
         elif headers.startswith("pod"):
             print(f"Importing ENERGY statistics from '{args.csv}'")
             luce_giornaliera(
                 args.csv,
                 state_meta_ids=[256, 257, 258],
-                statistic_meta_ids=[11, 12, 13],
+                statistics_meta_ids=[11, 12, 13],
                 update_since=datetime(2025, 10, 9),
             )
         else:
